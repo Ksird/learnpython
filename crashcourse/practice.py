61d3623f46d3672ce3723df2e18383379b46a5df
Ksird <rekafop@outlook.com>
2023-06-02 12:39:27 +0800

    3.1

--- crashcourse/practice.py
+++ crashcourse/practice.py
@@ -93,4 +93,40 @@ def practice_2_9():
     print(f"最喜欢的数是{number}")
 
 
-practice_2_9()
+def practice_3_1():
+    """
+    姓名：
+    将一些朋友的姓名储存在一个列表中，并将其命名为names。
+    依次访问该列表中的每个元素，从而将每个朋友的姓名打印出来。
+    """
+    names = ['xiuzui', 'abin', 'daot']
+    print(names[0])
+    print(names[1])
+    print(names[-1])
+
+
+def practice_3_2():
+    """
+    问候语：
+    继续使用练习3-1中的列表，但不打印每个朋友的姓名，而为每人打印一条消息。
+    每条消息都包含相同的问候语，但抬头为相应朋友的姓名
+    """
+    names = ['xiuzui', 'abin', 'daot']
+    print(f"Hello, {names[0]}")
+    print(f"Hello, {names[1]}")
+    print(f"Hello, {names[-1]}")
+
+
+def practice_3_3():
+    """
+    自己的列表：
+    想想你喜欢的通勤方式。如骑摩托车或开汽车，并创建一个包含多种方式的列表。
+    根据该列表打印一系列有关这些通勤方式的宣言，下面是一个例子。
+    I would like to own a Honda motorcycle.
+    """
+    commute = ['walk', 'car', 'motorcycle', 'bus', 'subway']
+    for i in commute:
+        print(f"I would like to {i}")
+
+
+practice_3_3()
